2317343d8dabd4c952182cbaa17de88db675688b
Alexander <shura.kolpakov.86@inbox.ru>
2019-03-06 21:30:41 +0300

    сделал easy

--- work_4.py
+++ work_4.py
@@ -1,2 +1,22 @@
-print("hello world")
-print('hello galaxy')
+# easy_1
+# import random
+#
+# lst_1 = [random.randint(-10, 10) for i in range(10)]
+# lst_2 = [i**2 for i in lst_1]
+# print(lst_1)
+# print(lst_2)
+
+# easy_2
+# fruits_1 = ['apple', 'orange', 'peach', 'mango']
+# fruits_2 = ['lemon', 'peach', 'banana', 'apple']
+# fruits_3 = [fruit for fruit in fruits_1 if fruit in fruits_2]
+# print(fruits_3)
+
+
+# easy_3
+# import random
+#
+# lst_1 = [random.randint(-10, 10) for i in range(10)]
+# lst_2 = [i for i in lst_1 if (i % 3 == 0 or i % 4 == 0) and i > 0]
+# print(lst_1)
+# print(lst_2)
